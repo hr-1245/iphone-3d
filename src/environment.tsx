e9047a00b74cfab26cb06a5ab980f1a22a2c441b
shehbaz <shehbaz@alphai.dev>
2025-03-24 14:23:46 +0500

    initial commit

---
+++ src/environment.tsx
@@ -0,0 +1,8 @@
+// To build the file
+// export const base_path ='/react/template/' 
+
+export const base_path ='/' 
+// To run the file in localhose
+// export const base_path ='/'
+ 
+export const img_path = '/'
